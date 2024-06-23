8cd13be3e046a3d90c3b546e85682dd1d2d97559
Roger Knight <roger@rogerknight.info>
2024-06-23 15:55:30 +0200

    Convert form to grid.

--- wp-content/themes/lumis-child/services-2024-page.php
+++ wp-content/themes/lumis-child/services-2024-page.php
@@ -32,9 +32,13 @@ get_header(); ?>
         }
         ?>
         <div class="service-introduction-content">
-          <p class="service-summary"><span><svg aria-hidden='true'><use xlink:href='/wp-content/themes/lumis-child/svg-defs.svg#tick'></use></svg></span><?php echo esc_html(
-            $service_introduction["service_summary"]
-          ); ?></p>
+          <p class="service-summary">
+            <span>
+              <svg aria-hidden='true'>
+                <use xlink:href='/wp-content/themes/lumis-child/svg-defs.svg#tick'></use>
+              </svg>
+            </span>
+          <?php echo esc_html($service_introduction["service_summary"]); ?></p>
           <?php echo wp_kses_post($service_introduction["service_introduction_block"]); ?>
         </div>
       </section>
